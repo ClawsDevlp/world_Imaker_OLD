b6e2be848689fe8994b311ba4de1ce49b3160744
BENHARIRA EVA <ebenhari@localhost.localdomain>
2019-12-18 17:15:17 +0100

    add_subdirectory pour third-party

---
+++ Build/CMakeFiles/world_Imaker.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/6im2/ebenhari/Documents/world_Imaker/Master/src/main.cpp" "/home/6im2/ebenhari/Documents/world_Imaker/Build/CMakeFiles/world_Imaker.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Master/lib/glad/include"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Master/lib/third-party/include"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Master/lib/imgui"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Master/lib"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Master/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/6im2/ebenhari/Documents/world_Imaker/Build/CMakeFiles/GLAD.dir/DependInfo.cmake"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Build/lib/glimac/CMakeFiles/glimac.dir/DependInfo.cmake"
+  "/home/6im2/ebenhari/Documents/world_Imaker/Build/CMakeFiles/IMGUI.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/world_Imaker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/world_Imaker.dir/src/main.cpp.o"
+  "world_Imaker.pdb"
+  "world_Imaker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/world_Imaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
